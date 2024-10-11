b6270fe60e7d9fb9ba317029daa416af68666c87
Marius Damm <mariusdamm1010@gmail.com>
2024-10-11 18:05:57 +0200

    feat (server): Add Spring Boot backend files

---
+++ server/src/main/java/de/marius/whereismystuff/ServerApplication.java
@@ -0,0 +1,13 @@
+package de.marius.whereismystuff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ServerApplication.class, args);
+  }
+
+}
